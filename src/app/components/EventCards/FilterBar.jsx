13007eb33735cf83e40377ff675faaec749181c6
brandon <41245867+murphybrandon@users.noreply.github.com>
2019-06-18 01:53:54 -0400

    refactored filterbar

--- src/app/components/EventCards/FilterBar.jsx
+++ src/app/components/EventCards/FilterBar.jsx
@@ -4,22 +4,17 @@ import React from 'react';
 // styled components
 import { FilterWrapper } from '../../styles/FilterBar.jsx';
 
-export default function FilterBar(props) {
-  const {
-    searchChange,
-    searchField,
-  } = props;
+const FilterBar = ({ searchChange, searchField }) => (
+  <>
+    <FilterWrapper>
+      <input
+        type="text"
+        placeholder="filter actions by name..."
+        onChange={searchChange}
+        value={searchField}
+      />
+    </FilterWrapper>
+  </>
+);
 
-  return (
-    <>
-      <FilterWrapper>
-        <input
-          type="text"
-          placeholder="filter actions by name..."
-          onChange={searchChange}
-          value={searchField}
-        />
-      </FilterWrapper>
-    </>
-  );
-}
+export default FilterBar;
